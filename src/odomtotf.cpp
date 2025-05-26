0eacaa3ca83d76270a06918ae45fba7321e85bff
Riccardo Dolci <riccardo2.dolci@mail.polimi.it>
2025-05-26 16:40:08 +0200

    update
    finally done tf for body_link

--- src/odomtotf.cpp
+++ src/odomtotf.cpp
@@ -1,47 +1,39 @@
-class odomtotf {
-public:
-    odomtotf() {
-        ros::NodeHandle private_nh("~");
-       
-        odometry_sub_ = nh_.subscribe("/odometry", 1, &odomtotf::odomtfCallback, this);
-        
-    }
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf/transform_broadcaster.h>
 
+class OdomToTF {
+public:
+    OdomToTF() {
+        odom_sub_ = nh_.subscribe("/odometry", 10, &OdomToTF::odomCallback, this);
+    }
 
 private:
     ros::NodeHandle nh_;
-    ros::Subscriber odometry_sub;
-    
-    tf::TransformBroadcaster odom_broadcaster_;
+    ros::Subscriber odom_sub_;
+    tf::TransformBroadcaster tf_broadcaster_;
 
-
-void speedsteerCallback(const geometry_msgs::PointStamped::ConstPtr& msg) {
-
-
-        double x = msg-->point.x;
-        double y = msg-->point.y;
-
+    void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
         geometry_msgs::TransformStamped odom_tf;
-        odom_tf.header.stamp = current_time;
-        odom_tf.header.frame_id = "world";
-        odom_tf.child_frame_id = "vehicle";
+        odom_tf.header.stamp = msg->header.stamp;
+        odom_tf.header.frame_id = "base_link";       // or "world" or your fixed frame
+        odom_tf.child_frame_id = "body_link";   // or "robot", whatever your robot's frame is
 
-        odom_tf.transform.translation.x = x;
-        odom_tf.transform.translation.y = y;
-        odom_tf.transform.translation.z = 0.0;
-        odom_tf.transform.rotation = odom_quat;
+        odom_tf.transform.translation.x = msg->pose.pose.position.x;
+        odom_tf.transform.translation.y = msg->pose.pose.position.y;
+        odom_tf.transform.translation.z = msg->pose.pose.position.z;
 
-        odom_broadcaster_.sendTransform(odom_tf);
+        odom_tf.transform.rotation = msg->pose.pose.orientation;
 
-
-}
-
+        tf_broadcaster_.sendTransform(odom_tf);
+    }
+};
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "odomtotf");
-    odomtotf node;
-    ROS_INFO("Nodo avviato correttamente!");
-
+    ros::init(argc, argv, "odom_to_tf");
+    OdomToTF node;
+    ROS_INFO("TF broadcaster from odometry started.");
     ros::spin();
     return 0;
 }
