25b3b11b26484362bc5db9a64b2dc4a8307ca936
Riccardo Dolci <riccardo2.dolci@mail.polimi.it>
2025-05-27 16:33:47 +0200

    Scan merge
    implemented scan merging from front and back lidar using ira library

--- src/odomtotf.cpp
+++ src/odomtotf.cpp
@@ -2,6 +2,8 @@
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <tf/transform_broadcaster.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 class OdomToTF {
 public:
@@ -33,6 +35,47 @@ private:
 int main(int argc, char** argv) {
     ros::init(argc, argv, "odom_to_tf");
     OdomToTF node;
+
+    static tf2_ros::StaticTransformBroadcaster static_broadcaster;
+    std::vector<geometry_msgs::TransformStamped> transforms;
+
+    // First static transform: base_link -> body_link
+    geometry_msgs::TransformStamped tf1;
+    tf1.header.stamp = ros::Time::now();
+    tf1.header.frame_id = "base_link";
+    tf1.child_frame_id = "body_link";
+    tf1.transform.translation.x = 0.0;
+    tf1.transform.translation.y = 0.0;
+    tf1.transform.translation.z = 0.0;
+
+    tf2::Quaternion q1;
+    q1.setRPY(0, 0, 0);
+    tf1.transform.rotation.x = q1.x();
+    tf1.transform.rotation.y = q1.y();
+    tf1.transform.rotation.z = q1.z();
+    tf1.transform.rotation.w = q1.w();
+
+    // Second static transform: base_link -> sick_back
+    geometry_msgs::TransformStamped tf2;
+    tf2.header.stamp = ros::Time::now();
+    tf2.header.frame_id = "base_link";
+    tf2.child_frame_id = "sick_back";
+    tf2.transform.translation.x = -0.27;
+    tf2.transform.translation.y = 0.0;
+    tf2.transform.translation.z = 0.0;
+
+    tf2::Quaternion q2;
+    q2.setRPY(0, 0, 3.14159); // 180 degrees in radians
+    tf2.transform.rotation.x = q2.x();
+    tf2.transform.rotation.y = q2.y();
+    tf2.transform.rotation.z = q2.z();
+    tf2.transform.rotation.w = q2.w();
+
+    // Add to vector and broadcast
+    transforms.push_back(tf1);
+    transforms.push_back(tf2);
+    static_broadcaster.sendTransform(transforms);
+
     ROS_INFO("TF broadcaster from odometry started.");
     ros::spin();
     return 0;
